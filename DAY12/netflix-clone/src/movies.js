0ee0d9bcb00e724df11f6572f9ae97fd6cb3d108
AKASHSINGH972 <akashsingh09072005@gmail.com>
2024-11-18 13:07:42 +0530

    push

---
+++ DAY12/netflix-clone/src/movies.js
@@ -0,0 +1,32 @@
+const movies=[{
+    id:1,
+    image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+    title:"Mission: Impossible – Dead Reckoning Part",
+    language:"English",
+    cost:'1055',
+
+},
+{
+    id: 2,
+    image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+    title:"Tarzan",
+    language:"Hindi",
+    cost:" 250",
+},
+{
+    id:3 ,
+    image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/9/d/d/2013-blu-ray-eagle-hindi-golmaal-original-imagms36uz2hucpy.jpeg?q=70",
+    title:"Golmaal",
+    language:"Hindi",
+    cost:" 300",
+},
+{
+    id:4 ,
+    image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/y/x/l/1985-dvd-eagle-hindi-don-amitabh-bachchan-original-imahfkagscjczgy2.jpeg?q=70",
+    title:"Don",
+    language:"Hindi",
+    cost:"250 ",
+}
+
+];
+export default movies;
