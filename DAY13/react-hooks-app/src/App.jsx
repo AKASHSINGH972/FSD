be22c2b36c1247212a4bacd95af4edbf441bb21d
AKASHSINGH972 <akashsingh09072005@gmail.com>
2024-12-02 13:06:55 +0530

    push

---
+++ DAY13/react-hooks-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react' 
+// import CounterApp from "./components/CounterApp";
+import ImageApp from './components/ImageApp';
+
+const App = () => {
+  return (
+    <>
+     <ImageApp/>
+    </>
+  
+  );
+};
+
+export default App
