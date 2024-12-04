a518653109496692b89151cfc3b1b147a9c1d096
AKASHSINGH972 <akashsingh09072005@gmail.com>
2024-12-04 12:30:01 +0530

    push

--- DAY13/react-hooks-app/src/App.jsx
+++ DAY13/react-hooks-app/src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react' 
-// import CounterApp from "./components/CounterApp";
+ import CounterApp from "./components/CounterApp";
 import ImageApp from './components/ImageApp';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import MainLayout from "./components/MainLayout";
 
 const App = () => {
-  return (
-    <>
-     <ImageApp/>
-    </>
+  return <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainLayout/>}>
+      <Route path="/counterapp" element={<CounterApp/>}/>
+      <Route path="/imageapp" element={<ImageApp/>}/>
+      </Route>
+      </Routes>
+      </BrowserRouter>
+  </div>
   
-  );
+  // (
+  // //   <>
+  // //    <ImageApp/>
+  // //   </>
+  
+  // // );
 };
 
 export default App
